752270aac0bdad1c33058ce515b857e0f59434ac
Rubem Dornas <rapdornas@gmail.com>
2025-04-20 09:40:06 -0400

    Atualizando arquivos

--- dicas_gestao_dados/R/clientes_produtos.R
+++ dicas_gestao_dados/R/clientes_produtos.R
@@ -1,12 +1,13 @@
 # Carregar pacotes necessários
 library(dplyr)
 library(tidyr)
+library(purrr)
 library(tictoc) # para contabilizar o tempo gasto
 
 tic()
 
 # Configurações para tamanho das tabelas
-n_clientes <- 1e6 # 1 milhão de clientes
+n_compras <- 1e6 # 1 milhão de compras
 n_produtos <- 100 # 100 categorias de produtos
 
 # Gerar tabela de produtos
@@ -18,10 +19,13 @@ produtos <- data.frame(
 # Gerar tabela de clientes ----
 set.seed(123) # Para reprodutibilidade
 
-clientes <- tibble(
+compras <- tibble(
   id = 1:1e6, # 1 milhão de clientes
-  nome = sample(c("João", "Maria", "José", "Carlos", "Ana", "Lucas", "Paula", "Fernanda", "Roberta", "Gustavo"), 1e6, replace = TRUE),
-  id_interesse = replicate(1e6, sample(1:100, sample(1:5, 1), replace = TRUE), simplify = FALSE) # Simula um array
-)
+  cliente = sample(c("João", "Maria", "José", "Carlos", "Ana", "Lucas", "Paula", "Fernanda", "Roberta", "Gustavo"), n_compras, replace = TRUE),
+  id_produto = replicate(n_compras, sample(1:100, sample(1:5, 1), replace = TRUE), simplify = FALSE) # Simula um array
+) |>
+  mutate(produtos = purrr::map_chr(id_produto, ~ paste(.x, collapse = ",")))
 
 toc()
+
+compras
--- dicas_gestao_dados/R/diamonds.R
+++ dicas_gestao_dados/R/diamonds.R
@@ -1,6 +1,25 @@
+# Example file for useful tools in RStudio
+# Using library ----
+
 library(ggplot2)
 
 diamonds|>
 ggplot(aes(x=carat,y=price,color=clarity))+
 geom_point()+
 labs(x="Carat",y="Price",title="Diamond Pricing")
+
+# Using four dots ----
+
+ggplot2::diamonds|>
+  ggplot2::ggplot(ggplot2::aes(x=carat,y=price,color=clarity))+
+  ggplot2::geom_point()+
+  ggplot2::labs(x="Carat",y="Price",title="Diamond Pricing")
+
+# Using use() ----
+
+use("ggplot2",c("diamonds","ggplot","aes","geom_point","labs"))
+
+diamonds|>
+  ggplot(aes(x=carat,y=price,color=clarity))+
+  geom_point()+
+  labs(x="Carat",y="Price",title="Diamond Pricing")
